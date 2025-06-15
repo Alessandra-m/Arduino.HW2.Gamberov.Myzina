7b0243f19317126b4634502d86ca9f27449c8c06
Alessandra-m <83640536+Alessandra-m@users.noreply.github.com>
2025-06-15 14:16:54 +0300

    Create ros_rgb.py

---
+++ Б/ros_rgb.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import threading
+import rospy
+from std_msgs.msg import ColorRGBA
+
+rospy.init_node('rgb_pub', anonymous=True)
+pub_ = rospy.Publisher('computer/traffic_light/cmd', ColorRGBA, queue_size=10)
+
+msg = ColorRGBA()
+
+
+msg.b = 0
+msg.r = 0
+msg.g = 0
+
+def pub():
+    global pub_, msg
+    pub.publish(msg)
+
+def talker():
+    global msg
+    pub = rospy.Publisher('computer/traffic_light/cmd', ColorRGBA, queue_size=10)
+    rospy.init_node('rgb_pub', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        msg = ColorRGBA()
+        char = input().lower()
+        msg.a = 0
+        if char == "r":
+            msg.r = 1
+            msg.g = 0
+            msg.b = 0
+        elif char == "g":
+            msg.g = 1
+            msg.r = 0
+            msg.b = 0
+        elif char == "b":
+            msg.b = 1
+            msg.r = 0
+            msg.g = 0
+        elif char == " ":
+            exit()
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
